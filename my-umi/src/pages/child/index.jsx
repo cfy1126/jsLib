0f56c4880bdf4196b9b6da77780d7dfcc04591ff
cao <cao@caodeMacBook-Air.local>
2024-06-01 09:36:01 +0800

    useContext、useMemo、useCallback

--- my-umi/src/pages/child/index.jsx
+++ my-umi/src/pages/child/index.jsx
@@ -1,8 +1,17 @@
-import { memo } from 'react';
-function Child({data}) {
+import { useContext, memo } from 'react';
+import MyContext from '../context';
+
+function Child({ data }) {
+  const { count, setCount } = useContext(MyContext);
+  console.log(MyContext);
   console.log(data);
-  //   console.log(data);
-  return <h1>子组件</h1>;
+  return (
+    <>
+      <h1>子组件</h1>
+      <p>count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+    </>
+  );
 }
 
 export default memo(Child);
--- my-umi/src/pages/index.jsx
+++ my-umi/src/pages/index.jsx
@@ -1,4 +1,5 @@
 import { useState, useCallback, useMemo } from 'react';
+import MyContext from './context'
 import { Button } from 'antd';
 import { Button as V2Button } from 'antd-mobile-v2';
 import { Button as V5Button } from 'antd-mobile';
@@ -8,34 +9,35 @@ import styles from './index.less';
 
 export default function IndexPage() {
   const [count, setCount] = useState(10);
-  const [value,setValue] = useState(null);
+  const [value, setValue] = useState(null);
   const [obj, setObj] = useState({ a: 10 });
   const onChangeCount = useCallback((value) => setCount(value), []);
-  const countFormat = useMemo(()=> {
+  const countFormat = useMemo(() => {
     console.log(count);
     return count * 10;
-  },[count])
+  }, [count]);
 
   return (
-    <div>
-      {/* <h1 className={styles.title}>组件</h1>
+    <MyContext.Provider value={{count,setCount}}>
+      <div>
+        {/* <h1 className={styles.title}>组件</h1>
       <Button type="primary">Button</Button>
       <V2Button type="primary">Button</V2Button>
       <V5Button color="primary" fill="solid">
         Solid
       </V5Button> */}
 
-      {/* 
+        {/* 
       <h1>图片资源引入</h1>
       <img src={bg} alt="bg"  width={100} height={100}/>
       <img src={require('../assets/favicon.ico')} alt="bg" width={100}/> */}
-      {/* 临时存放图片目录 */}
-      {/* <h1 style={{width: 100}} className='test'>样式内部使用</h1> */}
+        {/* 临时存放图片目录 */}
+        {/* <h1 style={{width: 100}} className='test'>样式内部使用</h1> */}
 
-      {/* 
+        {/* 
       <h1>全局样式和局部样式</h1>
       <a href="https://www.baidu.com" className='myLink title'>百度</a> */}
-      {/* <h1>混合</h1>
+        {/* <h1>混合</h1>
       <div id="a1">测试一</div>
       <div className="a2">测试二</div>
       <h1>嵌套</h1>
@@ -49,11 +51,13 @@ export default function IndexPage() {
         <p>测试六</p>
       </div> */}
 
-      <h1>父组件</h1>
+        {/* <h1>父组件</h1>
       <div>{countFormat}</div>
       <input type="text" onChange={(e)=> setValue(e.target.value)}/>
       <button onClick={() => setCount(count + 1)}>+1</button>
-      <Child data={obj} onchangeCount={onChangeCount} />
-    </div>
+      <Child data={obj} onchangeCount={onChangeCount} /> */}
+      </div>
+      <Child data={obj} onchangeCount={onChangeCount} /> 
+    </MyContext.Provider>
   );
 }
